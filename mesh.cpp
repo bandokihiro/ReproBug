86c1ae1b64891abe58442a59870a7a06c8b4df02
bandokihiro <bando.kihiro@gmail.com>
2020-04-09 14:28:56 -0700

    Remove try and catch of HDF5 API

--- mesh.cpp
+++ mesh.cpp
@@ -38,149 +38,132 @@ Mesh::Mesh(const toml::value &input_info) : partitioned(false) {
 }
 
 void Mesh::read_mesh(const string &mesh_file_name) {
-//    try {
-        // turn off the auto-printing when failure occurs
-        hsize_t dims[2]; // buffer to store size in each dimensions
+    // turn off the auto-printing when failure occurs
+    hsize_t dims[2]; // buffer to store size in each dimensions
 
-        // open hdf5 file
-        H5File file(mesh_file_name, H5F_ACC_RDONLY);
+    // open hdf5 file
+    H5File file(mesh_file_name, H5F_ACC_RDONLY);
 
-        // fetch all scalars first
-        dims[0] = 1;
-        int rank = 1;
-        DataSpace mspace(rank, dims);
+    // fetch all scalars first
+    dims[0] = 1;
+    int rank = 1;
+    DataSpace mspace(rank, dims);
 
-        // number of spatial dimensions
-        DataSet dataset   = file.openDataSet(DSET_DIMS);
-        DataSpace dataspace = dataset.getSpace();
-        dataset.read(&dim, PredType::NATIVE_INT, mspace, dataspace);
-        // number of elements
-        dataset   = file.openDataSet(DSET_NELEM);
-        dataspace = dataset.getSpace();
-        dataset.read(&nElem, PredType::NATIVE_INT, mspace, dataspace);
-        // number of nodes
-        dataset   = file.openDataSet(DSET_NNODE);
-        dataspace = dataset.getSpace();
-        dataset.read(&nNode, PredType::NATIVE_INT, mspace, dataspace);
-        // number of nodes per element
-        dataset   = file.openDataSet(DSET_NNODE_PER_ELEM);
-        dataspace = dataset.getSpace();
-        dataset.read(&nNode_per_elem, PredType::NATIVE_INT, mspace, dataspace);
-        // number of interior faces
-        dataset   = file.openDataSet(DSET_NIFACE);
-        dataspace = dataset.getSpace();
-        dataset.read(&nIface, PredType::NATIVE_INT, mspace, dataspace);
-        // geometric order of the mesh
-        dataset   = file.openDataSet(DSET_QORDER);
-        dataspace = dataset.getSpace();
-        dataset.read(&order, PredType::NATIVE_INT, mspace, dataspace);
+    // number of spatial dimensions
+    DataSet dataset   = file.openDataSet(DSET_DIMS);
+    DataSpace dataspace = dataset.getSpace();
+    dataset.read(&dim, PredType::NATIVE_INT, mspace, dataspace);
+    // number of elements
+    dataset   = file.openDataSet(DSET_NELEM);
+    dataspace = dataset.getSpace();
+    dataset.read(&nElem, PredType::NATIVE_INT, mspace, dataspace);
+    // number of nodes
+    dataset   = file.openDataSet(DSET_NNODE);
+    dataspace = dataset.getSpace();
+    dataset.read(&nNode, PredType::NATIVE_INT, mspace, dataspace);
+    // number of nodes per element
+    dataset   = file.openDataSet(DSET_NNODE_PER_ELEM);
+    dataspace = dataset.getSpace();
+    dataset.read(&nNode_per_elem, PredType::NATIVE_INT, mspace, dataspace);
+    // number of interior faces
+    dataset   = file.openDataSet(DSET_NIFACE);
+    dataspace = dataset.getSpace();
+    dataset.read(&nIface, PredType::NATIVE_INT, mspace, dataspace);
+    // geometric order of the mesh
+    dataset   = file.openDataSet(DSET_QORDER);
+    dataspace = dataset.getSpace();
+    dataset.read(&order, PredType::NATIVE_INT, mspace, dataspace);
 
-        // resize internal structures
-        eptr.resize(nElem + 1);
-        eind.resize(nElem * nNode_per_elem);
-        coord.resize(nNode);
-        IFace_to_elem.resize(nIface);
+    // resize internal structures
+    eptr.resize(nElem + 1);
+    eind.resize(nElem * nNode_per_elem);
+    coord.resize(nNode);
+    IFace_to_elem.resize(nIface);
 
-        elem_num_IFace.resize(nElem);
-        elem_to_IFace.resize(nElem);
-        elem_num_BFace.resize(nElem);
-        elem_to_BFace.resize(nElem);
-        for (int i = 0; i < nElem; i++){
-            elem_num_IFace[i] = 0.0;
-            elem_to_IFace[i].resize(6); // max number of faces
-            elem_num_BFace[i] = 0.0;
-            elem_to_BFace[i].resize(6); // max number of faces
+    elem_num_IFace.resize(nElem);
+    elem_to_IFace.resize(nElem);
+    elem_num_BFace.resize(nElem);
+    elem_to_BFace.resize(nElem);
+    for (int i = 0; i < nElem; i++){
+        elem_num_IFace[i] = 0.0;
+        elem_to_IFace[i].resize(6); // max number of faces
+        elem_num_BFace[i] = 0.0;
+        elem_to_BFace[i].resize(6); // max number of faces
+    }
+
+    // fetch elemID->nodeID
+    dims[0] = nElem;
+    dims[1] = nNode_per_elem;
+    rank = 2;
+    mspace = DataSpace(rank, dims);
+    dataset   = file.openDataSet(DSET_ELEM_TO_NODES);
+    dataspace = dataset.getSpace();
+    // ordering: elemID X nodeID (last is fastest)
+    dataset.read(eind.data(), PredType::NATIVE_INT, mspace, dataspace);
+
+    // fetch node coordinates
+    vector<rtype> buff(dim * nNode, 0.);
+    dims[0] = nNode;
+    dims[1] = dim;
+    rank = 2;
+    mspace = DataSpace(rank, dims);
+    dataset = file.openDataSet(DSET_NODE_COORD);
+    dataspace = dataset.getSpace();
+    // ordering: nodeID X coordinates (last is fastest)
+    dataset.read(buff.data(), PredType::NATIVE_DOUBLE, mspace, dataspace);
+    // fill coordinates
+    for (int iNode = 0; iNode<nNode; iNode++) {
+        coord[iNode].resize(dim);
+        for (int idim=0; idim<dim; idim++) {
+            coord[iNode][idim] = buff[iNode*dim + idim];
         }
+    }
 
-        // fetch elemID->nodeID
-        dims[0] = nElem;
-        dims[1] = nNode_per_elem;
-        rank = 2;
-        mspace = DataSpace(rank, dims);
-        dataset   = file.openDataSet(DSET_ELEM_TO_NODES);
-        dataspace = dataset.getSpace();
-        // ordering: elemID X nodeID (last is fastest)
-        dataset.read(eind.data(), PredType::NATIVE_INT, mspace, dataspace);
+    // fetch IFace->elem and IFace->node
+    vector<int> buff_int;
+    buff_int.resize(nIface * 6);
+    dims[0] = nIface;
+    dims[1] = 6;
+    rank = 2;
+    mspace = DataSpace(rank, dims);
+    dataset = file.openDataSet(DSET_IFACE);
+    dataspace = dataset.getSpace();
+    dataset.read(buff_int.data(), PredType::NATIVE_INT, mspace, dataspace);
+    for (int i=0; i<nIface; i++) {
+        IFace_to_elem[i].resize(6);
+        IFace_to_elem[i][0] = buff_int[6*i + 0]; // left element ID
+        IFace_to_elem[i][1] = buff_int[6*i + 1]; // face ID for the left element
+        IFace_to_elem[i][2] = buff_int[6*i + 2]; // orientation for the left element
+        IFace_to_elem[i][3] = buff_int[6*i + 3]; // right element ID
+        IFace_to_elem[i][4] = buff_int[6*i + 4]; // face ID for the right element
+        IFace_to_elem[i][5] = buff_int[6*i + 5]; // orientation for the right element
 
-        // fetch node coordinates
-        vector<rtype> buff(dim * nNode, 0.);
-        dims[0] = nNode;
-        dims[1] = dim;
-        rank = 2;
-        mspace = DataSpace(rank, dims);
-        dataset = file.openDataSet(DSET_NODE_COORD);
-        dataspace = dataset.getSpace();
-        // ordering: nodeID X coordinates (last is fastest)
-        dataset.read(buff.data(), PredType::NATIVE_DOUBLE, mspace, dataspace);
-        // fill coordinates
-        for (int iNode = 0; iNode<nNode; iNode++) {
-            coord[iNode].resize(dim);
-            for (int idim=0; idim<dim; idim++) {
-                coord[iNode][idim] = buff[iNode*dim + idim];
-            }
-        }
+        // Set reverse mapping
+        elem_to_IFace[buff_int[6*i + 0]][elem_num_IFace[buff_int[6*i + 0]]] = i;
+        elem_to_IFace[buff_int[6*i + 3]][elem_num_IFace[buff_int[6*i + 3]]] = i;
+        elem_num_IFace[buff_int[6*i + 0]]++;
+        elem_num_IFace[buff_int[6*i + 3]]++;
+    }
 
-        // fetch IFace->elem and IFace->node
-        vector<int> buff_int;
-        buff_int.resize(nIface * 6);
-        dims[0] = nIface;
-        dims[1] = 6;
-        rank = 2;
-        mspace = DataSpace(rank, dims);
-        dataset = file.openDataSet(DSET_IFACE);
-        dataspace = dataset.getSpace();
-        dataset.read(buff_int.data(), PredType::NATIVE_INT, mspace, dataspace);
-        for (int i=0; i<nIface; i++) {
-            IFace_to_elem[i].resize(6);
-            IFace_to_elem[i][0] = buff_int[6*i + 0]; // left element ID
-            IFace_to_elem[i][1] = buff_int[6*i + 1]; // face ID for the left element
-            IFace_to_elem[i][2] = buff_int[6*i + 2]; // orientation for the left element
-            IFace_to_elem[i][3] = buff_int[6*i + 3]; // right element ID
-            IFace_to_elem[i][4] = buff_int[6*i + 4]; // face ID for the right element
-            IFace_to_elem[i][5] = buff_int[6*i + 5]; // orientation for the right element
+    // fill eptr that indicates where data for node i in eind is
+    idx_t counter = 0;
+    for (int i = 0; i < nElem + 1; i++) {
+        eptr[i] = counter;
+        counter += nNode_per_elem;
+    }
 
-            // Set reverse mapping
-            elem_to_IFace[buff_int[6*i + 0]][elem_num_IFace[buff_int[6*i + 0]]] = i;
-            elem_to_IFace[buff_int[6*i + 3]][elem_num_IFace[buff_int[6*i + 3]]] = i;
-            elem_num_IFace[buff_int[6*i + 0]]++;
-            elem_num_IFace[buff_int[6*i + 3]]++;
-        }
+    // resize partition ID container
+    elem_part_id.resize(nElem);
+    node_part_id.resize(nNode);
 
-        // fill eptr that indicates where data for node i in eind is
-        idx_t counter = 0;
-        for (int i = 0; i < nElem + 1; i++) {
-            eptr[i] = counter;
-            counter += nNode_per_elem;
-        }
-
-        // resize partition ID container
-        elem_part_id.resize(nElem);
-        node_part_id.resize(nNode);
-
-        // read boundary faces
-        if (BFG_names.size()>0) {
-            read_boundary_faces(file);
-        }
-        else {
-            nBFG = 0;
-            nBFace = 0;
-        }
-//    }
-//
-//        // catch failure caused by the H5File operations
-//    catch( FileIException error ) {
-//        error.printErrorStack();
-//    }
-//
-//        // catch failure caused by the DataSet operations
-//    catch( DataSetIException error ) {
-//        error.printErrorStack();
-//    }
-//
-//        // catch failure caused by the DataSpace operations
-//    catch( DataSpaceIException error ) {
-//        error.printErrorStack();
-//    }
+    // read boundary faces
+    if (BFG_names.size()>0) {
+        read_boundary_faces(file);
+    }
+    else {
+        nBFG = 0;
+        nBFace = 0;
+    }
 }
 
 void Mesh::read_boundary_faces(H5::H5File &file) {
